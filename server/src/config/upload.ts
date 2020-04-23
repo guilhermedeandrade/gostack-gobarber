fcce8a86540d931b44a594194277e088949b0b0c
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-04-23 18:23:41 +0100

    chore: improve code quality

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -5,7 +5,7 @@ import crypto from 'crypto'
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
 export default {
-  directory: path.resolve(__dirname, '..', '..', 'tmp'),
+  directory: tmpFolder,
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename: (_req, file, cb) => {
--- server/src/repositories/AppointmentsRepository.ts
+++ server/src/repositories/AppointmentsRepository.ts
@@ -1,13 +1,9 @@
-import { find } from 'remeda'
-import { isEqual } from 'date-fns'
 import { EntityRepository, Repository } from 'typeorm'
 
 import { Appointment } from '../models'
 
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
-  private appointments: Appointment[]
-
   public async findByDate(date: Date): Promise<Appointment | null> {
     const findAppointment = await this.findOne({ where: { date } })
 
