d30a6bfd6909d7cf890b9fd0e133605ce8dd64ee
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-07-07 00:54:00 +0100

    feat: integrate with AWS S3

--- server/src/shared/container/providers/index.ts
+++ server/src/shared/container/providers/index.ts
@@ -1,32 +1,3 @@
-import { container } from 'tsyringe'
-import mailConfig from '@config/mail'
-
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
-import IStorageProvider from './StorageProvider/models/IStorageProvider'
-
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
-import SESMailProvider from './MailProvider/implementations/SESMailProvider'
-import IMailProvider from './MailProvider/models/IMailProvider'
-
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider'
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider'
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-)
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-)
-
-const MAIL_PROVIDERS = {
-  ethereal: container.resolve(EtherealMailProvider),
-  ses: container.resolve(SESMailProvider),
-}
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  MAIL_PROVIDERS[mailConfig.driver],
-)
+import '../providers/StorageProvider'
+import '../providers/MailTemplateProvider'
+import '../providers/MailProvider'
