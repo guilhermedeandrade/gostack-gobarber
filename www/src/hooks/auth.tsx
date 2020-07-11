b3ebb4fc523311a7fb847d6ca97ff9cea2126f7d
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-07-11 17:11:36 +0100

    feat: add base structure for the Dashboard page

--- www/src/hooks/auth.tsx
+++ www/src/hooks/auth.tsx
@@ -2,9 +2,15 @@ import React, { createContext, useCallback, useState, useContext } from 'react'
 
 import api from '../services/api'
 
+interface User {
+  id: string
+  name: string
+  avatar_url: string
+}
+
 interface AuthState {
   token: string
-  user: object
+  user: User
 }
 
 interface SignInCredentials {
@@ -15,7 +21,7 @@ interface SignInCredentials {
 interface AuthContextData {
   signIn: (credentials: SignInCredentials) => Promise<void>
   signOut: () => void
-  user: object
+  user: User
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData)
--- www/src/pages/Dashboard/index.tsx
+++ www/src/pages/Dashboard/index.tsx
@@ -1,5 +1,151 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { FiPower, FiClock } from 'react-icons/fi'
+import DayPicker, { DayModifiers } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>
+import { useAuth } from '../../hooks/auth'
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles'
+
+import logoImg from '../../assets/logo.svg'
+
+const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const { signOut, user } = useAuth()
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day)
+    }
+  }, [])
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber logo" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Welcome,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Scheduled appointments</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 6</span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next appointment</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/8797405?s=400&u=d4f57bff8140358062ba5cd006bf53ff3305f1a3&v=4"
+                alt="Guilherme de Andrade"
+              />
+              <strong>Guilherme de Andrade</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/45701839?s=460&u=97fd508578d5b069c417369b2ff3015511effab5&v=4"
+                  alt="Camila Pimentel"
+                />
+
+                <strong>Camila Pimentel</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/45701839?s=460&u=97fd508578d5b069c417369b2ff3015511effab5&v=4"
+                  alt="Camila Pimentel"
+                />
+
+                <strong>Camila Pimentel</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Afternoon</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                15:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/45701839?s=460&u=97fd508578d5b069c417369b2ff3015511effab5&v=4"
+                  alt="Camila Pimentel"
+                />
+
+                <strong>Camila Pimentel</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            fromMonth={new Date()}
+            disabledDays={{ daysOfWeek: [0, 6] }}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            onDayClick={handleDateChange}
+            selectedDays={selectedDate}
+            weekdaysShort={['S', 'M', 'T', 'W', 'T', 'F', 'S']}
+          />
+        </Calendar>
+      </Content>
+    </Container>
+  )
+}
 
 export default Dashboard
